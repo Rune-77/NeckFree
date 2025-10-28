a6a05b14d434e2dfdf57c24b8633505a7f74497e
jhs <tlswlgns3281@naver.com>
2025-10-29 03:15:38 +0900

    Add Spring Boot server module (neckfree-server)

---
+++ neckfree-server/src/test/java/com/neckfree/neckfree/server/NeckFreeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neckfree.neckfree.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeckFreeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
